b2b850a8a87ab97146f4304cdb671e4c7ffe430d
cdcd <caodan01@sohu.com>
2014-01-27 10:20:41 +0800

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,3 +1,4 @@
+#"#"符号是注释，注释多行要用‘’‘  ’‘’
 # A comment,this is so you can read your program later.
 #Anything after the # is ignored by python.
 
@@ -7,3 +8,8 @@ print 'I could have code like this.'  #and the comment after is ignored
 #print "This won't run"
 
 print "This will run."
+
+'''
+You can also use a comment to 'disable' or comment out a piece of code:
+print "This won't run"
+'''
