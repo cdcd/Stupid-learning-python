9e3562f26e4841dc5b93a8d2edf577ee32c1af41
cdcd <caodan01@sohu.com>
2014-01-27 10:35:48 +0800

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,4 +1,5 @@
-#打印
+#encoding=utf-8
+#打印,打印多行加'''   '''
 print 'Hello World!'
 print 'Hello Again'
 print 'I like typing this'
@@ -6,8 +7,27 @@ print 'this is fun'
 print "I'd much rather you 'not'"
 print 'I"said" do not touch this.'
 
+print '\n',
 print '''aaa
 bbbb
 cccc 
 '''
 print "1234#123"
+
+'''
+result:
+
+Hello World!
+Hello Again
+I like typing this
+this is fun
+I'd much rather you 'not'
+I"said" do not touch this.
+
+aaa
+bbbb
+cccc 
+
+1234#123
+
+'''
