3a5502b5e69857acd25e86daa03d951295cd2fbe
cdcd <caodan01@sohu.com>
2014-01-27 10:36:41 +0800

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,3 +1,6 @@
+#encoding=utf-8
+#变量
+
 cars = 100
 space_in_a_car = 4.0
 drivers = 30
@@ -8,9 +11,9 @@ carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passerngers / cars_driven
 
 
-print "There are",cars,"cars available."
-print "There are %d cars available" % cars
-print "There are %d cars available and %d drivers available" % (cars,drivers)
+print "There are",cars,"cars available."      #打印一个变量
+print "There are %d cars available" % cars    #打印一个变量
+print "There are %d cars available and %d drivers available" % (cars,drivers)  #打印多个变量
 print "There are only",drivers,"drivers available."
 print "There will be",cars_not_drivern,"empty cars today."
 print "we can transport",carpool_capacity,"people today."
@@ -18,3 +21,17 @@ print "We have",passerngers,"to carpool today."
 print "We need to put about",average_passengers_per_car,"in each car."
 
 
+'''
+result:
+
+There are 100 cars available.
+There are 100 cars available
+There are 100 cars available and 30 drivers available
+There are only 30 drivers available.
+There will be 70 empty cars today.
+we can transport 120.0 people today.
+We have 90 to carpool today.
+We need to put about 3 in each car.
+
+
+'''
